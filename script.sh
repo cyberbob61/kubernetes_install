2cab5777159d80ef6a8be3f48cec70ea549d9af5
Stepan Iudin <cyberbob61@gmail.com>
2025-03-29 23:55:55 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
+#network configuration
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
 
 #swap
 sudo swapoff -a
-sudo sed -i '/ swap / s/^/#/' /etc/fstab
+sudo sed -i '/swap/d' /etc/fstab
 
 #routing
-echo -e "net.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/10-k8s.conf > /dev/null
+echo -e "net.bridge.bridge-nf-call-ip6tables = 1\nnet.bridge.bridge-nf-call-iptables = 1\nnet.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/10-k8s.conf > /dev/null
 sudo sysctl -f /etc/sysctl.d/10-k8s.conf
-
-#containerd
-sudo apt-get update
-sudo apt-get install -y containerd
-sudo systemctl enable containerd
-sudo systemctl start containerd
 
 #apt kubernetes
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
@@ -26,3 +26,17 @@ sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 #kubernetes packages install
 sudo apt-get update -y
 sudo apt-get install -y kubeadm kubectl kubelet
+
+#containerd
+sudo apt-get update
+sudo apt-get install -y containerd
+sudo systemctl enable containerd
+sudo systemctl start containerd
+
+cat > /etc/crictl.yaml << _EOF
+runtime-endpoint: unix:///var/run/containerd/containerd.sock
+_EOF
+
+#finish
+echo "done"
+
